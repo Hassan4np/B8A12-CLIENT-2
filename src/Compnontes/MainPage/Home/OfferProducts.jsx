14fdcf5c84797dbd9d1750908ddb62a15a8a4ed6
Hassan Ali <hassan4np@gmail.com>
2023-12-13 11:29:54 +0600

    update

--- src/Compnontes/MainPage/Home/OfferProducts.jsx
+++ src/Compnontes/MainPage/Home/OfferProducts.jsx
@@ -23,14 +23,14 @@ const OfferProducts = () => {
             </div>
              <div className="w-[278px] h-[170px] border rounded-xl  " style={{ backgroundImage: `url(${p3})` }}>
                 <div className="text-center mt-4">
-                    <h1 className="text-[14px] font-medium uppercase text-black">Summer Sale</h1>
+                    <h1 className="text-[14px] font-medium uppercase text-black">Winter Sale</h1>
     
                     <h5 className="text-base pb-7 pt-3 font-normal text-black">Up to <span className="text-[18PX] rounded-md font-semibold bg-black px-3 py-[6px] text-[#FF8A00]">64% OFF</span></h5>    
                 </div>
             </div>
              <div className="w-[278px] h-[170px] border rounded-xl  " style={{ backgroundImage: `url(${p4})` }}>
                 <div className="text-center mt-4">
-                    <h1 className="text-[14px] font-medium uppercase text-black">Summer Sale</h1>
+                    <h1 className="text-[14px] font-medium uppercase text-black">Winter Sale</h1>
                   
                     <h5 className="text-base pb-7 pt-3 font-normal text-white">Up to <span className="text-[18PX] rounded-md font-semibold bg-black px-3 py-[6px] text-[#FF8A00]">55% OFF</span></h5>    
                 </div>
